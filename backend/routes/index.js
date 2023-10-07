9b09eaf52c8787a0d91ff81cc403b5be027ebdd1
Aaron <aj.aaron.jenson@gmail.com>
2023-10-06 18:28:41 -0600

    index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,11 @@
+// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api');
 
-router.use('/api', apiRouter);
-
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.status(201).json({});
-  });
-}
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 module.exports = router;
+//end
